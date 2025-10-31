114b967cfe1e83b0b971f6b9b6731a06e8314189
Trainernoster <leo.berger309@gmail.com>
2025-10-31 15:53:31 +0100

    Change CLI

--- Python_Test_1/receive.py
+++ Python_Test_1/receive.py
@@ -2,6 +2,7 @@
 import pika # Python librarby based on the AMQR protocol
 import datetime # Get current time
 import time # Sleep library
+import sys # System library to get CLI access 
 
 # RabbitMQ connection
 credentials = pika.PlainCredentials('admin', 'RabbitAdmin')                     # Credentials for the Rabbitmq user
@@ -14,13 +15,28 @@ channel = connection.channel()
 # Declare a queue (creates if it doesn't exist)
 channel.queue_declare(queue='test_queue')
 
+# create a update function for the time
+def updateTime(message_time):
+    sys.stdout.write("\033[K")                          # Delete the old message
+    # Get current time
+    current_time = datetime.datetime.now()
+    clock = current_time.strftime("%H:%M:%S.%f")[:-2]
+    
+    # Print message
+    print(f"Received:")
+    print(f"            Time from server: {message_time}")
+    print(f"           Time from machine: {clock}")
+    sys.stdout.write("\033[F\033[F\033[F")                  # Set courser to message top
+
+# Callback if message arrives
 def callback(ch, method, properties, body):
-    print(f"Received: {body.decode()}")
+    updateTime(body.decode())
 
 # Subscribe to the queue
 channel.basic_consume(queue='test_queue',
                       on_message_callback=callback,
                       auto_ack=True)
 
+# End
 print('Waiting for messages. To exit press CTRL+C')
 channel.start_consuming()
