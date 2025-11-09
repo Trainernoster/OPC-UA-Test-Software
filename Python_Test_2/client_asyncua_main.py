7adf7d0826e567a9c8727f03e543cbf4c0feaa7b
Trainernoster <leo.berger309@gmail.com>
2025-11-09 16:39:02 +0100

    changes in client

--- Python_Test_2/client_asyncua_main.py
+++ Python_Test_2/client_asyncua_main.py
@@ -1,11 +1,21 @@
 import asyncio
-from OPC_UA_Client import OPCUAClient
+from asyncua import Client
 
 async def main():
-    opc_ua_client = OPCUAClient()
-    await opc_ua_client.start_Client()
-    print(await opc_ua_client.get_First_Value())
+    url = "opc.tcp://192.168.50.52:4840/freeopcua/server/"
+    async with Client(url=url) as client:
+        print(f"Connected to {url}")
+        
+        # Get the node we want to read
+        var = client.get_node("ns=3;i=1")
+        
+        i = 1
+        while True:
+            i += 1
+            value = await var.read_value()
+            print(f"Read MyVariable = {value}")
+            await var.write_value(i)
+            await asyncio.sleep(2)
 
 if __name__ == "__main__":
-    """ Run the main function. """
     asyncio.run(main())
