4c4cee2fd279b81df510c3dfbf615d61dc17b8fb
Trainernoster <leo.berger309@gmail.com>
2025-11-12 10:46:16 +0100

    changes in programm loop

--- Python_Test_1/send.py
+++ Python_Test_1/send.py
@@ -2,6 +2,7 @@
 import pika # Python librarby based on the AMQR protocol
 import datetime # Get current time
 import time # Sleep library
+import sys # Deleting
 
 # RabbitMQ connection
 credentials = pika.PlainCredentials('admin', 'RabbitAdmin')                     # Credentials for the Rabbitmq user
@@ -14,19 +15,25 @@ channel = connection.channel()
 # Declare a queue (creates if it doesn't exist)
 channel.queue_declare(queue='test_queue')
 
-# Time loop
-for i in range(60):
-    # Get current time
-    current_time = datetime.datetime.now()
-    clock = current_time.strftime("%H:%M:%S.%f")[:-2]
-    message = f"{clock}"
-    
-    # Send message
-    channel.basic_publish(exchange='',
-                          routing_key='test_queue',
-                          body=message)
-    print(f"Sent: {message}")
+# Main programm loop
+print("Press Ctrl+C to stop.")
+try:
+    while True:
+        # Get current time
+        current_time = datetime.datetime.now()
+        clock = current_time.strftime("%H:%M:%S.%f")[:-2]
+        message = f"{clock}"
 
-    time.sleep(1)
+        # Send message
+        channel.basic_publish(exchange='',
+                                routing_key='test_queue',
+                                body=message)
+        print(f"Sent: {message}")
+        time.sleep(1)
+        sys.stdout.write("\033[F")                  # Set courser to message top
+
+except KeyboardInterrupt:
+    print("Stopping server...")
+
 
 connection.close()
