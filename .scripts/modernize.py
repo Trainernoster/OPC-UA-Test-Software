b6a3c58be3acc278d52705928e688ab7bddf32c5
Trainernoster <leo.berger309@gmail.com>
2025-11-20 16:45:38 +0100

    change in schript modernize

--- .scripts/modernize.py
+++ .scripts/modernize.py
@@ -30,29 +30,24 @@ def main():
 
 
     if os.path.isfile(SOURCE_LIST_FILE):
-        print(f"Editing file: {SOURCE_LIST_FILE}")
 
         with open(SOURCE_LIST_FILE, "r", encoding="utf-8") as f:
             lines = f.readlines()
 
         if not lines:
-            print("File is empty, nothing to edit.")
+            ...
         else:
             first_line = lines[0]
 
-            # remove the first character
             if first_line[0] == '#':
                 modified_line = first_line[1:] if len(first_line) > 1 else ""
             else:
                 modified_line = first_line
 
-            # overwrite file with ONLY the modified first line
             with open(SOURCE_LIST_FILE, "w", encoding="utf-8") as f:
                 f.write(modified_line)
-
-            print("First character removed and all other lines deleted.")
     else:
-        print(f"File does not exist: {target_file}")
+        print(f"File does not exist: {SOURCE_LIST_FILE}")
 
     print("Modernization completed successfully!")
 
