295aafb0f6ccf154642639eea63121f9712ef081
Trainernoster <leo.berger309@gmail.com>
2025-11-20 16:43:25 +0100

    change in schript modernize

--- .scripts/modernize.py
+++ .scripts/modernize.py
@@ -8,13 +8,10 @@ Script to install modernize apt and Debian.
 
 To make the script executable run:
     chmod +x install_python312.py
-in the command line or runn it with python.
+in the command line or run it with python.
 """
 
-PYTHON_VERSION = "3.12.10"
-SRC_DIR = "/usr/src"
-TAR_FILE = f"Python-{PYTHON_VERSION}.tgz"
-URL = f"https://www.python.org/ftp/python/{PYTHON_VERSION}/{TAR_FILE}"
+SOURCE_LIST_FILE = "/etc/apt/sources.list.d/modernize.list" 
 
 def run(cmd):
     """Run shell command and exit if it fails."""
@@ -26,11 +23,36 @@ def run(cmd):
 
 def main():
     # update and upgrade
-    run("sudo apt update")
-    run("sudo apt upgrade -y")
-    run("sudo apt full-upgrade -y")
-    run("sudo apt modernize-sources")
-    run("cd /etc/apt/sources.list.d")
+    #run("sudo apt update")
+    #run("sudo apt upgrade -y")
+    #run("sudo apt full-upgrade -y")
+    #run("sudo apt modernize-sources")
+
+
+    if os.path.isfile(SOURCE_LIST_FILE):
+        print(f"Editing file: {SOURCE_LIST_FILE}")
+
+        with open(SOURCE_LIST_FILE, "r", encoding="utf-8") as f:
+            lines = f.readlines()
+
+        if not lines:
+            print("File is empty, nothing to edit.")
+        else:
+            first_line = lines[0]
+
+            # remove the first character
+            if first_line[0] == '#':
+                modified_line = first_line[1:] if len(first_line) > 1 else ""
+            else:
+                modified_line = first_line
+
+            # overwrite file with ONLY the modified first line
+            with open(SOURCE_LIST_FILE, "w", encoding="utf-8") as f:
+                f.write(modified_line)
+
+            print("First character removed and all other lines deleted.")
+    else:
+        print(f"File does not exist: {target_file}")
 
     print("Modernization completed successfully!")
 
